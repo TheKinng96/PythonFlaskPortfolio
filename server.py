a660f70dcaba43ac2c36aabfc181fad3bf1d86d6
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-06-06 20:01:50 +0900

    fixed the basic links problems of the original files

--- server.py
+++ server.py
@@ -1,3 +1,18 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
+@app.route('/')
+def homepage():
+    return render_template("index.html")
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+# @app.route('/contact.html', methods=['POST', 'GET'])
+# def contact():
+#     return render_template('Contact.html')
+
