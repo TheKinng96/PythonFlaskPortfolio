6386a7054a391c9a541b1842fdaaed597d262340
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-06-06 20:54:35 +0900

    created db to store messages

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import csv
+
 app = Flask(__name__)
 
 
@@ -12,7 +14,31 @@ def html_page(page_name):
     return render_template(page_name)
 
 
-# @app.route('/contact.html', methods=['POST', 'GET'])
-# def contact():
-#     return render_template('Contact.html')
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f'\n{email},{subject}\n{message}\n')
 
+
+def write_to_csv(data):
+    with open('database.csv', mode='a') as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',', newline="", quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def contact():
+    if request.method == "POST":
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect("/thanks.html")
+        except:
+            return 'did not save to database'
+    else:
+        return 'something wrong, please try again.'
